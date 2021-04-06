32d376983378527977c661689d6a7103c0d0f677
Attila Kormos <attila.kormos@dmgmori-heitec.hu>
2021-04-06 15:18:22 +0200

    Add palindrom exercise

---
+++ src/main/java/hu/heitec/interview/chardonnay/palindrom/Palindrom.java
@@ -0,0 +1,9 @@
+package hu.heitec.interview.chardonnay.palindrom;
+
+public class Palindrom {
+
+	public boolean isPalindrom(String input) {
+		throw new UnsupportedOperationException("Not implemented!");
+	}
+
+}
---
+++ src/test/java/hu/heitec/interview/chardonnay/palindrom/PalindromTest.java
@@ -0,0 +1,33 @@
+package hu.heitec.interview.chardonnay.palindrom;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class PalindromTest {
+
+	private Palindrom undertest;
+
+	@ParameterizedTest
+	@ValueSource(strings = {
+		"abba",
+		"beka!akeb",
+		"][_(a(_[]",
+		"aaaa!!aaaa",
+	})
+	public void testValidPalindroms(String input) {
+		Assertions.assertTrue(undertest.isPalindrom(input));
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {
+		"alma",
+		"beka",
+		"labda!adba",
+		"][_.([])]_,",
+	})
+	public void testInvalidPalindroms(String input) {
+		Assertions.assertFalse(undertest.isPalindrom(input));
+	}
+
+}
