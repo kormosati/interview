d40bba8f5d7134c1879414b54a4391e88b30c0e4
Attila Kormos <attila.kormos@dmgmori-heitec.hu>
2021-04-06 17:01:36 +0200

    Test palindrom for empty string

--- src/test/java/hu/heitec/interview/chardonnay/palindrom/PalindromTest.java
+++ src/test/java/hu/heitec/interview/chardonnay/palindrom/PalindromTest.java
@@ -1,12 +1,18 @@
 package hu.heitec.interview.chardonnay.palindrom;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class PalindromTest {
 
 	private Palindrom undertest;
+
+	@Test
+	public void testForEmptyString() {
+		Assertions.assertTrue(undertest.isPalindrom(""));
+	}
 
 	@ParameterizedTest
 	@ValueSource(strings = {
